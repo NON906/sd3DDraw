80bd82a9f0a1b8bcff6f527b8a1ab70ffe3c009e
NON906 <mumeigames@gmail.com>
2023-11-18 17:31:04 +0900

    依存関係の更新。自動でのレイヤー追加

--- sd3DDraw/Assets/sd3DDraw/Editor/InitTarget.cs
+++ sd3DDraw/Assets/sd3DDraw/Editor/InitTarget.cs
@@ -33,6 +33,56 @@ namespace SD3DDraw
             return true;
         }
 
+        static bool settingLayers(string[] layerNames, int[] defaultLayersNo)
+        {
+            bool isChange = false;
+
+            string[] managerText = File.ReadAllLines("ProjectSettings/TagManager.asset");
+            int managerTextLayersStart = 0;
+            foreach (string line in managerText)
+            {
+                if (line == "  layers:")
+                {
+                    managerTextLayersStart++;
+                    break;
+                }
+                managerTextLayersStart++;
+            }
+
+            for (int loop = 0; loop < layerNames.Length; loop++)
+            {
+                if (LayerMask.NameToLayer(layerNames[loop]) >= 0)
+                {
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(LayerMask.LayerToName(defaultLayersNo[loop])))
+                {
+                    managerText[managerTextLayersStart + defaultLayersNo[loop]] = "  - " + layerNames[loop];
+                    isChange = true;
+                    continue;
+                }
+
+                for (int loop2 = managerTextLayersStart + 8; loop2 < managerTextLayersStart + 32; loop2++)
+                {
+                    if (managerText[loop2] == "  - ")
+                    {
+                        managerText[loop2] = "  - " + layerNames[loop];
+                        isChange = true;
+                        break;
+                    }
+                }
+            }
+
+            if (isChange)
+            {
+                File.WriteAllLines("ProjectSettings/TagManager.asset", managerText);
+                AssetDatabase.Refresh();
+            }
+
+            return isChange;
+        }
+
         public int callbackOrder => 0;
 
         public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget)
@@ -46,6 +96,8 @@ namespace SD3DDraw
                 AssetDatabase.CopyAsset("Assets/sd3DDraw/Jsons/package_local.json", "Packages/online.mumeigames.RemoveBackground@0.1.0/package.json");
                 AssetDatabase.Refresh();
             }
+
+            settingLayers(new string[] { "SDTarget" }, new int[] { 8 });
         }
     }
 }
